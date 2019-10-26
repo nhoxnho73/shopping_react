c9f6bfc1bec868857315f88e4eed4518a1430740
nhoxnho73 <50539278+nhoxnho73@users.noreply.github.com>
2019-10-26 20:09:45 +0700

    fix node

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
     <Router>
       <Header/>
-      <Route path="/" exact component={About}/>
+      <Route path="/" exact component={About} />
       <Route path="/products" component={Products}/>
       <Footer/>
     </Router>
